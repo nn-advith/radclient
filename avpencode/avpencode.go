67a91912a425f37d0df83f66913ea925e87356fa
nn-advith <advith.nn@gmail.com>
2025-10-01 19:40:39 +0530

    DEV: password encoding as per rfc; basic udp stream to test

--- avpencode/avpencode.go
+++ avpencode/avpencode.go
@@ -1,6 +1,7 @@
 package avpencode
 
 import (
+	"crypto/md5"
 	"log"
 	"net"
 	"strconv"
@@ -38,6 +39,7 @@ func EncodeIP(value AVPValue, _ AVPContext) []uint8 {
 	return pip.To4()
 }
 
+// for int32 i.e avps that are integers with 4 octet value fields. write some other for dynamic ints ig
 func EncodeInteger32(value AVPValue, _ AVPContext) []uint8 {
 	i, err := strconv.Atoi(value.(string))
 	if err != nil {
@@ -54,6 +56,37 @@ func EncodeInteger32(value AVPValue, _ AVPContext) []uint8 {
 // arg0: password, arg1: requestauth, arg2: secret
 func EncodePassword(value AVPValue, ctx AVPContext) []uint8 {
 	//calculate here
-	return []uint8{}
+	// length At least 18 and no larger than 130.
+	// value The String field is between 16 and 128 octets long, inclusive.
+
+	// procedure
+	// check if length is multiple of 16 octet. if no, pad to next multiple and divide to blocks
+	// sequentially calculate
+	// c1 = p1 XOR MD5(secret + auth)
+	// ci = pi XOR MD5(secret + ci-1)
+	// return c1+c2+...+ci (concatenated)
+	ptext := []uint8(value.(string))
+
+	if len(ptext)%16 != 0 {
+		// pad here
+		ptext = append(ptext, make([]uint8, 16-len(ptext)%16)...)
+	}
+	opstr := []uint8{}
+	for l := 0; l < len(ptext); l += 16 {
+		// fmt.Printf("%x-", ptext[l:l+16])
+		var temp []uint8
+		if l == 0 {
+			temp = append([]uint8(ctx.Secret), []uint8(ctx.Authenticator[:])...)
+		} else {
+			temp = append([]uint8(ctx.Secret), opstr[l-16:l]...)
+		}
+		md5op := md5.Sum(temp)
+		xorop := make([]uint8, 16)
+		for i := 0; i < 16; i++ {
+			xorop[i] = ptext[l+i] ^ md5op[i]
+		}
+		opstr = append(opstr, xorop...)
+	}
+	return opstr
 
 }
